96cfeb6ae558395782ba50f5e7c199cc3f1e089b
Lukáš Pánek <lplukaslukas@hotmail.com>
2017-01-05 19:37:13 +0100

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,26 @@
+document.write("Ahoj, ");
+datboi = "*insert name here*";
+document.write(datboi);
+cislo = 7;
+cislo = cislo*2;
+document.write(cislo);
+alert("Weeaboo nalezen! Pravdepodobna pozice: po tve pravici!");
+prompt("Fuq yo");
+
+cislo = prompt("Zadej cislo od 1 do 100");
+if (cislo <= 100){
+    alert("Diky!")
+}
+else{
+    alert("No.")
+}
+var max;
+for(var i = 0;i<5;i++){
+    vstup = prompt("Zadej cislo: ")
+    if(vstup>max){
+        max = vstup;
+    }
+
+
+}
+alert("Nejvetsi cislo je "+max);
